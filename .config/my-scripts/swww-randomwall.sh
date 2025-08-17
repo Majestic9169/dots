b37c96986f7afe458418af90014323b99809367d
Tegan Jain <teganjain527@gmail.com>
2025-08-17 14:04:54 +0530

    feat: add wallpaper selector to rofi

--- .config/my-scripts/swww-randomwall.sh
+++ .config/my-scripts/swww-randomwall.sh
@@ -5,17 +5,11 @@ THEME=animegirls
 wallpapersDir="$HOME/Wallpapers/$THEME/"
 
 # Get a list of all image files in the wallpapers directory
-wallpapers=("$wallpapersDir"/*)
-
-# Check if the wallpapers array is empty
-if [ ${#wallpapers[@]} -eq 0 ]; then
-    # If the array is empty, refill it with the image files
-    wallpapers=("$wallpapersDir"/*)
-fi
+wallpapers=($(ls $wallpapersDir))
 
 # Select a random wallpaper from the array
 wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
-selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+selectedWallpaper="$wallpapersDir/${wallpapers[$wallpaperIndex]}"
 
 # env variables to control transition
 SWWW_TRANSITION_DURATION=3
@@ -24,10 +18,7 @@ SWWW_TRANSITION=random
 # Update the wallpaper using the swww img command
 echo "[~] Selected Wallpaper is $selectedWallpaper"
 swww img "$selectedWallpaper" --transition-duration ${SWWW_TRANSITION_DURATION} -t ${SWWW_TRANSITION}
-wal -i $selectedWallpaper
+wal -i "$selectedWallpaper"
 killall -SIGUSR2 waybar
 cp ~/.cache/wal/config ~/.config/cava/config
 pkill -USR2 cava
-
-# Remove the selected wallpaper from the array
-unset "wallpapers[$wallpaperIndex]"
